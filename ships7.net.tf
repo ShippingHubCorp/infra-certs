2fbc0028c2df361a375be91e29f11a97aaae02ad
Shinichi Urano <s@urano.me>
2025-01-29 22:13:02 -0500

    add ships7 and nightly workflow

--- ships7.net.tf
+++ ships7.net.tf
@@ -1,6 +1,6 @@
-module "ssl_shippinghub7" {
+module "ssl_ships7" {
   source = "github.com/cloudkeep-terraform-modules/aws-letsencrypt-cert?ref=v0.1.0"
-  common_name = "*.shippinghub7.net"
+  common_name = "*.ships7.net"
   email = "f-wong@shippinghub7.net"
   s3_bucket_name = "shippinghub-certs"
 }
--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,12 @@ terraform {
       version = "~> 2.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "sh-tfstate-infra-certs"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "sh-tfstate-infra-certs"
+  }
 }
